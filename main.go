ab396cb459d49f7310c9f3de49569709967918ec
phaseharry <harrychenhc@icloud.com>
2026-08-09 20:20:41 -0400

    fix gosec security issues

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,10 +91,13 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 2 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	portClean := strings.ReplaceAll(port, "\n", "")
+	portClean = strings.ReplaceAll(portClean, "\r", "")
+	log.Printf("Serving on port: %s\n", portClean)
 	log.Fatal(srv.ListenAndServe())
 }
